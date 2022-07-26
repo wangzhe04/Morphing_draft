af273eb7df24d3c30ea1c2b68a5a3ab8fc768b07
Zhe <wangzhe04052000@gmail.com>
2022-07-25 23:43:38 -0500

    Added Drafts.py, n_param=4 runnable

---
+++ Drafts.py
@@ -0,0 +1,206 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from prettytable import PrettyTable
+
+
+class Morpher:
+
+    def __init__(self, n_parameters=2):
+        self.components = None
+        self.n_components = None
+        self.basis = None
+        self.morphing_matrix = None
+        self.n_benchmarks = None
+        self.n_parameters = n_parameters
+        self.morphing_matrix_component_weights = None
+        self.morphing_weights = None
+        self.W_i = None
+        self.matrix_before_invertion = None
+        self.this_xsec = None
+
+    # the power of each g1^n * g2^n, n_components is the number of components/coupling pairs
+    def set_components(self, components):
+        self.components = components
+        self.n_components = len(components)
+
+    # basis, each row is a benchmark, Example: g1_1 = basis[0, 0], g2_1 = basis[0, 1]
+    def set_basis(self, basis):
+        self.basis = basis
+        self.n_benchmarks = len(basis)
+
+    def calculate_morphing_matrix(self):
+        inv_morphing_submatrix = np.zeros([self.n_benchmarks, self.n_components])
+        for b in range(self.n_benchmarks):
+            for c in range(self.n_components):
+                factor = 1.0
+                for p in range(self.n_parameters): # n_parameters == 2
+                    factor *= float(self.basis[b, p] ** self.components[c, p]) # get value of each g1^n * g2^n
+                inv_morphing_submatrix[b, c] = factor
+        morphing_submatrix = inv_morphing_submatrix.T
+        self.matrix_before_invertion = morphing_submatrix
+        # QR factorization
+        q, r= np.linalg.qr(morphing_submatrix, 'complete')
+        self.morphing_matrix = np.dot(np.linalg.pinv(r), q.T)
+        return self.morphing_matrix
+
+    # calculate W_i with W_i = w_i*sigma_i
+    def calculate_weights_times_crossection(self, xsec):
+        index = len(self.morphing_weights)
+        if len(xsec) < index:
+            raise Exception('The number of xsec values is smaller than the number of morphing weights')
+        
+        # Get the corresponding xsec values for the morphing weights    
+        this_xsec = xsec[:index]
+        self.this_xsec = this_xsec
+        self.W_i = np.multiply(this_xsec, self.morphing_weights, dtype=np.float32)
+        return self.W_i
+
+    # calculate Neff = sum(W_i)
+    def calculate_Neff(self):
+        return sum(self.W_i)
+    
+    # calculate N_tot = sum(abs(W_i))
+    def calculate_Ntot(self):
+        return sum(np.abs(self.W_i))
+
+    def calculate_Ntot_squared(self):
+        return sum(self.W_i * self.W_i)
+
+    # calculate the morphing weights
+    def calculate_morphing_weights(self, theta):
+        component_weights = np.zeros(self.n_components)
+        for c in range(self.n_components):
+            factor = 1.0
+            for p in range(self.n_parameters):
+                factor *= float(theta[p] ** self.components[c, p])
+            component_weights[c] = factor
+
+        component_weights = np.array(component_weights)
+        self.morphing_matrix_component_weights = component_weights
+
+        self.morphing_weights = np.dot(self.morphing_matrix, component_weights)
+        return self.morphing_weights
+
+    # get basis points with ranges
+    def get_predict_points(self, g1 = 1, g2_ranges = [-13, 13]):
+        g2_list = list(range(g2_ranges[0], g2_ranges[1] + 1))
+        
+        n_basis= len(g2_list)
+        basis_points = []
+        for i in range(n_basis):
+            g2 = g2_list[i]
+            basis_point = [g1, g2]
+            basis_points.append(basis_point)
+        return np.array(basis_points)
+
+    def get_predict_xsec(self, predict_points, know_xsec, known_basis, this_components):
+        morpher = Morpher(self.n_parameters)
+        morpher.set_components(this_components)
+        morpher.set_basis(known_basis)
+        morpher.calculate_morphing_matrix()
+
+        res_xsec = []
+        for i in range(len(predict_points)):
+            this_point = predict_points[i]
+            morpher.calculate_morphing_weights(this_point)
+            this_xsec = morpher.calculate_weights_times_crossection(know_xsec)
+            this_xsec = morpher.calculate_Neff()
+            res_xsec.append(this_xsec)
+        return np.array(res_xsec)
+
+    # return in a list with the order corresponging to the predict points [small -> large]
+    def get_Neff_Ntot(self, predict_points, know_xsec, known_basis, this_components):
+        morpher = Morpher(self.n_parameters)
+        morpher.set_components(this_components)
+        morpher.set_basis(known_basis)
+        morpher.calculate_morphing_matrix()
+
+        res_Neff_Ntot = []
+        for i in range(len(predict_points)):
+            this_point = predict_points[i]
+            morpher.calculate_morphing_weights(this_point)
+            this_xsec = morpher.calculate_weights_times_crossection(know_xsec)
+            this_Neff = morpher.calculate_Neff()
+            this_Ntot = morpher.calculate_Ntot()
+            res_Neff_Ntot.append(this_Neff/this_Ntot)
+        # print(res_Neff_Ntot)
+
+        return np.array(res_Neff_Ntot)
+
+    def get_Neff_Ntot_squared(self, predict_points, know_xsec, known_basis, this_components):
+        morpher = Morpher(self.n_parameters)
+        morpher.set_components(this_components)
+        morpher.set_basis(known_basis)
+        morpher.calculate_morphing_matrix()
+
+        res_Neff_Ntot_squared = []
+        for i in range(len(predict_points)):
+            this_point = predict_points[i]
+            morpher.calculate_morphing_weights(this_point)
+            this_xsec = morpher.calculate_weights_times_crossection(know_xsec)
+            this_Neff = morpher.calculate_Neff()
+            this_Neff_squared = this_Neff * this_Neff
+            this_Ntot_Squared = morpher.calculate_Ntot_squared()
+            res_Neff_Ntot_squared.append(this_Neff_squared/this_Ntot_Squared)
+
+        return np.array(res_Neff_Ntot_squared)
+
+    def get_predict_points_with_range(self, sample_size = 1000, g1 = 1, g2_range = [-10, 10]):
+        g2_list = []
+        for i in range(sample_size):
+            g2 = random.uniform(g2_range[0], g2_range[1])
+            g2_list.append(g2)
+        g2_list.sort()
+        n_basis= len(g2_list)
+        basis_points = []
+        for i in range(n_basis):
+            g2 = g2_list[i]
+            basis_point = [g1, g2]
+            basis_points.append(basis_point)
+        return g2_list, np.array(basis_points)
+
+    # add a xsec value to the input xsec list, which will not affect self.xsec.
+    # This method is mainly used for adding non consecutive neff values to the xsec list.
+    def add_predict_Neff(self, changing_xsec, input_xsec, add_g2_values, this_basis, n_components):
+
+        if type(add_g2_values) == list or type(add_g2_values) == np.ndarray:
+            for i in add_g2_values:
+                g2_ranges = [i, i]
+                changing_xsec = np.append(changing_xsec, self.get_predict_xsec(self.get_predict_points(g1 = 1, g2_ranges = g2_ranges), input_xsec, this_basis, n_components))
+            res = np.array(changing_xsec)
+        elif(type(add_g2_values) == int):
+            g2_ranges = [add_g2_values, add_g2_values]
+            res = np.append(changing_xsec, self.get_predict_xsec(self.get_predict_points(g1 = 1, g2_ranges = g2_ranges), input_xsec, this_basis, n_components))
+        else:
+            print("add_g2_values type error")
+            res = np.array(changing_xsec)
+        return res
+
+
+
+
+
+if __name__=="__main__":
+    """
+    The code blow compare xsex value with n_base=5 and n_base=7 as well as simulated values. 
+"""
+
+    # The code below shows teh w_i, xsec, and W_i for the given
+    this_components = np.array([[2, 2, 0, 0], [2, 1, 1, 0], [2, 1, 0, 1], [2, 0, 2, 0], [2, 0, 1, 1], [2, 0, 0, 2]])
+    this_basis = np.array([[1, -5 , 1, 2], [1, -4, 3, 4], [1, -3, 5, 6], [1, -2, 7, 8], [1, -1, 9, 8], [1, 0, 7, 6], [1,1, 3, 4]]) # basis
+    xsec = np.array([0.515, 0.732, 0.527, 0.742, 0.354, 0.527, 0.364, 0.742, 0.364, 0.621, 0.432, 0.621, 0.432]) # define once, the code will take the corresponding xsec values for the morphing weights
+    predict_point = np.array([1, -10, 3, 4] )  # change the point to predict
+
+    morpher = Morpher(n_parameters=3)
+    morpher.set_components(this_components)
+    morpher.set_basis(this_basis)
+    print(morpher.calculate_morphing_matrix())
+    morpher.calculate_morphing_weights(predict_point)
+
+
+
+ 
+
+#%%
