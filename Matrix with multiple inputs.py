eb68c12f13460768f6a7fc6ace550d06dc556c0d
Zhe <wangzhe04052000@gmail.com>
2022-08-08 16:51:45 -0500

    implementing get matrix with max_powers

--- Matrix with multiple inputs.py
+++ Matrix with multiple inputs.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sympy import Poly, symbols, expand
+from numpy import linalg as la
 
 class Morpher:
 
@@ -11,18 +12,18 @@ class Morpher:
         self.n_parameters = n_parameters
         self.gp = None # production
         self.gd = None # decay
-        self.gc =None # combine
+        self.gs =None # combine
 
     # basis, each row is a benchmark, Example: g1_1 = basis[0, 0], g2_1 = basis[0, 1]
-    def set_basis(self, basis_p = None, basis_d = None, basis_c = None):
+    def set_basis(self, basis_p = None, basis_d = None, basis_s = None):
 
-        if basis_p is None and basis_d is None and basis_c is None:
+        if basis_p is None and basis_d is None and basis_s is None:
             raise Exception('No basis is given')
 
-        if basis_c is not None:  
-            self.basis = basis_c
-            self.gc = basis_c
-            self.n_benchmarks = len(basis_c[0])
+        if basis_s is not None:  
+            self.basis = basis_s
+            self.gs = basis_s
+            self.n_benchmarks = len(basis_s[0])
 
         if basis_p is not None:
             self.gp = basis_p
@@ -32,16 +33,16 @@ class Morpher:
             self.gd = basis_d
             self.n_benchmarks = len(basis_d[0])
 
-        if basis_c is not None and basis_p is not None and basis_d is not None:
-            assert len(basis_p[0]) == len(basis_d[0]) == len(basis_c[0]), "the number of basis points in production, decay and combine should be the same"
+        if basis_s is not None and basis_p is not None and basis_d is not None:
+            assert len(basis_p[0]) == len(basis_d[0]) == len(basis_s[0]), "the number of basis points in production, decay and combine should be the same"
             self.n_benchmarks = len(basis_p[0])
             return
-        elif basis_c is not None and basis_p is not None:
-            assert len(basis_p[0]) == len(basis_c[0]), "the number of basis points in production and combine should be the same"
+        elif basis_s is not None and basis_p is not None:
+            assert len(basis_p[0]) == len(basis_s[0]), "the number of basis points in production and combine should be the same"
             self.n_benchmarks = len(basis_p[0])
             return
-        elif basis_c is not None and basis_d is not None:
-            assert len(basis_d[0]) == len(basis_c[0]), "the number of basis points in decay and combine should be the same"
+        elif basis_s is not None and basis_d is not None:
+            assert len(basis_d[0]) == len(basis_s[0]), "the number of basis points in decay and combine should be the same"
             self.n_benchmarks = len(basis_d[0])
             return
         elif basis_p is not None and basis_d is not None:
@@ -49,22 +50,29 @@ class Morpher:
             self.n_benchmarks = len(basis_p[0])
             return
 
+    # get the minimal number of indepedent samples
+    def get_Nmin(self, ns, np, nd):
+        res1 = (ns*(ns+1) * (ns+2) * ((ns+3) + 4 * (np+nd)))/24
+        res2 = (ns*(ns+1) * np*(np+1) + ns*(ns+1)*nd*(nd+1) + np*(np+1)*nd*(nd+1))/4 
+        res3 = ns*np*nd*(ns+np+nd+3)/2
+        return res1 + res2 + res3
+
     def calculate_morphing_matrix_multiple_coupling(self):
         n_gp = 0
         n_gd = 0
-        n_gc = 0
+        n_gs = 0
 
         if self.gp is not None:
             n_gp = len(self.gp) # n_gp == n for total of gp_1 ... gp_n
         if self.gd is not None:
             n_gd = len(self.gd)
-        if self.gc is not None:
-            n_gc = len(self.gc)
+        if self.gs is not None:
+            n_gs = len(self.gs)
         
         assert self.components is not None, "No components are given"
 
         # the first n_gd components are for gd, the next n_gp components in self.compoents are for gp, the last n_gc components are for gc
-        assert (n_gp + n_gd + n_gc) == len(self.components[0]), "The number of coupling parameters in basis is not equal to the number of components"
+        assert (n_gp + n_gd + n_gs) == len(self.components[0]), "The number of coupling parameters in basis is not equal to the number of components"
 
         inv_morphing_submatrix = np.zeros([self.n_benchmarks, self.n_components])
 
@@ -80,16 +88,16 @@ class Morpher:
                             factor *= float(self.gp[i,b] ** self.components[c,i+n_gd] )
                         else:
                             factor *= float(self.gp[i,b] ** self.components[c,i])
-                if n_gc != 0: # if gc coupling exists
-                    for k in range(n_gc):
+                if n_gs != 0: # if gc coupling exists
+                    for k in range(n_gs):
                         if n_gd != 0 and n_gp != 0: # add the length of gd and gp to index if they are not none
-                            factor *= float(self.gc[k,b] ** self.components[c,k+n_gd+n_gp])
+                            factor *= float(self.gs[k,b] ** self.components[c,k+n_gd+n_gp])
                         elif n_gd != 0:
-                            factor *= float(self.gc[k,b] ** self.components[c,k+n_gd])
+                            factor *= float(self.gs[k,b] ** self.components[c,k+n_gd])
                         elif n_gp != 0:
-                            factor *= float(self.gc[k,b] ** self.components[c,k+n_gp])
+                            factor *= float(self.gs[k,b] ** self.components[c,k+n_gp])
                         else:
-                            factor *= float(self.gc[k,b] ** self.components[c,k])
+                            factor *= float(self.gs[k,b] ** self.components[c,k])
                 inv_morphing_submatrix[b, c] = factor
         # print("inv_morphing_submatrix:\n", inv_morphing_submatrix.T)
         morphing_submatrix = inv_morphing_submatrix.T
@@ -100,7 +108,7 @@ class Morpher:
         return self.morphing_matrix
 
 
-    def find_components_with_multiple_couplings(self, Nd = 0, Np = 0, Ns = 0):
+    def find_components_with_multiple_couplings(self, max_overall_power = 0, Nd = 0, Np = 0, Ns = 0):
         lst = []
 
         if Np < 0 or Nd < 0 or Ns < 0:
@@ -156,10 +164,22 @@ if __name__=="__main__":
     # n_parameters here should equal to n_d + n_p + n_c
     morpher = Morpher(n_parameters=2)
 
+    # Print minimum number of samples needed
+    print("minimum number of samples required:\n", morpher.get_Nmin(n_s, n_p, n_d))
+
+    # Print the couplings
+    if gd is not None:
+        print("gd:\n", gd.T)
+    if gp is not None:
+        print("gp:\n", gp.T)
+    if gs is not None:
+        print("gs:\n", gs.T)
+
     this_components = morpher.find_components_with_multiple_couplings(Nd = n_d, Np = n_p, Ns = n_s)
     print("Powers of components:\n", this_components)
-    morpher.set_basis( basis_p=gp, basis_d=gd, basis_c = gs)
+    morpher.set_basis( basis_p=gp, basis_d=gd, basis_s = gs)
     print("Matrix:\n",morpher.calculate_morphing_matrix_multiple_coupling())
+    print("Condition number:\n", la.cond(morpher.morphing_matrix, 1))
 
  
 
